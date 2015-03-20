d954d47b2b6e946fdb00ffd91984b45cd1db5e3c
BOZii <justinboswell@live.com>
2015-03-20 00:31:14 -0400

    fixed signup

---
+++ Server/README.md
@@ -0,0 +1,3 @@
+Whiteboard - CMSC 491 Mobile Application Development Project
+
+by Justin Boswell, Ivan Grinkevich, and Jason Kruse
--- Server/config/passport.js
+++ Server/config/passport.js
@@ -98,7 +98,7 @@ module.exports = function(passport) {
                 // if there is no user with that email
                 var ext = email.split("@")[1];
                 console.log(ext);
-                if(ext !== 'vcu.edu' || ext !== 'mymail.vcu.edu'){
+                if(ext !== 'vcu.edu' && ext !== 'mymail.vcu.edu'){
                     return done(null, false, req.flash('signupMessage', 'That email is not a VCU domain.'))
                 }else{
                     // create the user
