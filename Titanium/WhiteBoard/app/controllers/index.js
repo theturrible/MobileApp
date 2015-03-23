c4a629152c28bee9fec8454cb33f9c59c4bf1c5a
Ivan Grinkevich <igrinkevich@sibereg.com>
2015-03-22 23:26:04 -0400

    added first api call to login

--- Titanium/WhiteBoard/app/controllers/index.js
+++ Titanium/WhiteBoard/app/controllers/index.js
@@ -8,3 +8,42 @@ function doClick(e) {
 
 $.index.open();
 
+$.btnLogin.addEventListener('click',function(e)
+{
+   Titanium.API.info("Login Button Clicked");
+   if($.txtLogin.value == "" || $.txtPass.value == ""){
+   	 alert("Please fill out username/password fields");
+	}else{
+	//we gunna call apiii!!!
+		
+		var httpClient = Ti.Network.createHTTPClient(
+			{
+				timeout: 10000
+			}
+		);
+		httpClient.onload = function(){
+			var responseJson = JSON.parse(httpClient.responseText);
+			alert(responseJson.id);
+		
+		};
+		httpClient.onerror = function(){
+			var responseJson = JSON.parse(httpClient.responseText);
+			Titanium.API.log(responseJson);
+		
+		};
+		httpClient.open('POST', 'http://localhost:8080/api/login');
+		httpClient.setRequestHeader('Content-Type', 'application/json');
+		
+		var data = {
+			email: $.txtLogin.value,
+			password: $.txtPass.value,
+			role: "student"
+			
+		};
+		Titanium.API.log("UserName: " + $.txtLogin.value);
+		Titanium.API.log("UserName: " + $.txtPass.value);
+		
+		httpClient.send(JSON.stringify(data));
+		
+	}
+});
