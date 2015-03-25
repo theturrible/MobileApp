a626730fa135da13b463a7ff696065fb6381591a
BOZii <justinboswell@live.com>
2015-03-25 02:03:58 -0400

    merges.. auth tokens done for api

--- Server/server.js
+++ Server/server.js
@@ -8,19 +8,18 @@ var port     = process.env.PORT || 8080;
 var mongoose = require('mongoose');
 var passport = require('passport');
 var flash    = require('connect-flash');
-var path = require('path');
+var path 	 = require('path');
 
 var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
+var jwt 		 = require('jwt-simple');
 
 var configDB = require('./config/database.js');
 
 // configuration ===============================================================
 mongoose.connect(configDB.url); // connect to our database
-
- require('./config/passport')(passport); // pass passport for configuration
 
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
@@ -28,6 +27,7 @@ app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser()); // get information from html forms
 
 app.set('view engine', 'ejs'); // set up ejs for templating
+app.set('tokenSecret', 'vcurams');
 
 // required for passport
 app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
@@ -36,8 +36,10 @@ app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 app.use(express.static(path.join(__dirname, 'public')));
 
+require('./config/passport')(passport, jwt); // pass passport for configuration
+
 // routes ======================================================================
-require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+require('./app/routes.js')(app, passport, jwt); // load our routes and pass in our app and fully configured passport
 
 // launch ======================================================================
 app.listen(port);
