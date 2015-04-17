98850a1d52fc54a87f195f7a589a4184676f4128
BOZii <justinboswell@live.com>
2015-04-16 23:05:12 -0400

    removed unwanted code

--- Server/public/js/course.js
+++ Server/public/js/course.js
@@ -1,19 +1,3 @@
 $(document).ready(function(){
-	$('#del').on('click', function(){
-		
-		$.ajax({
-   				url: 'http://api.joind.in/v2.1/talks/10889',
-   				data: {
-      				format: 'json'
-   				},
-   				error: function() {
-      				$('#info').html('<p>An error has occurred</p>');
-   				},
-   				dataType: 'json',
-   				success: function(data) {
-      				
-   				},
-   				type: 'DELETE'
-		});
-	});
+	
 });
